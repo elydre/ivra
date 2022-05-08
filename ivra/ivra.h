c8c8b43584fc57a7df250ffb2a7e341a20a0e72d
pf4 <74263128+elydre@users.noreply.github.com>
2022-05-08 22:43:32 +0200

    Update ivra.h

--- ivra/ivra.h
+++ ivra/ivra.h
@@ -1,11 +1,10 @@
-﻿// ivra.h : fichier Include pour les fichiers Include système standard,
-// ou les fichiers Include spécifiques aux projets.
+﻿// Include
 
 #pragma once
 
 #include <iostream>
 
-// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
+// en-têtes
 
 class IVRA {
 public:
