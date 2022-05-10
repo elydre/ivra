6af626e674ea67a10d15b0a4ed4e16775f5aef65
pf4 <74263128+elydre@users.noreply.github.com>
2022-05-10 12:15:45 +0200

    0.1.2

--- ivra/interpreter.cpp
+++ ivra/interpreter.cpp
@@ -17,13 +17,25 @@ void start_inter(vector<string> code, IVRA obj) {
 		else if (mode == "show") {
 			obj.show(stoi(args[1]));
 		}
+
+		else if (mode == "add") {
+			obj.add(stoi(args[1]), stoi(args[2]), stoi(args[3]));
+		}
+
+		else if (mode == "sub") {
+			obj.sub(stoi(args[1]), stoi(args[2]), stoi(args[3]));
+		}
+
+		else if (mode == "mul") {
+			obj.mul(stoi(args[1]), stoi(args[2]), stoi(args[3]));
+		}
+
+		else if (mode == "div") {
+			obj.div(stoi(args[1]), stoi(args[2]), stoi(args[3]));
+		}
+
+		else if (mode == "is_egal") {
+			obj.is_egal(stoi(args[1]), stoi(args[2]), stoi(args[3]));
+		}
 	}
 }
-
-/* TODO *
-add
-sub
-mul
-div
-is_egal
-*/
--- ivra/ivra.cpp
+++ ivra/ivra.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	string work_dir = "C:/Users/pf4/fichiers/GitHub/elydre/ivra/ivra/";
+	string work_dir = "/home/pf4/Documents/GitHub/ivra/ivra/";
 	vector<string> sortie = start_parse(work_dir + "code.ivr");
 	
 	IVRA obj;
